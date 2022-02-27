66fff6d8917c3082a806436c4f9ac7e5672a5085
Larry Le <lle114@student.hbuhsd.edu>
2022-02-26 20:23:45 -0800

    Fixed Everything

--- Main.py
+++ Main.py
@@ -2,22 +2,72 @@
 
 from dotenv import load_dotenv
 import os
+import discord
 
 load_dotenv()
 discordToken = os.getenv("Token")
 
-from discord.ext import commands
-bot = commands.Bot(command_prefix = "nib")
+from discord.ext import commands, tasks
+bot = commands.Bot(command_prefix = "uh ")
 
-@bot.command(name = "math")
-async def add(context, num1, num2):
-    
-    result = (int(num1) + int(num2))
-    await context.reply(f"{num1} + {num2} is " + str(result) + " ffs")
-
+# Use with discord.ext tasks.loop to doing things like update bot presence 
+from itertools import cycle
+cycleStatus = cycle(["Sketching", "Polynomials", "Simulator"])
 
 # Functions --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+@tasks.loop(seconds = 8)
+async def playGame():
+    await bot.change_presence(activity = discord.Game(next(cycleStatus)))
+
+@bot.event
+async def on_ready():
+    print(f"{bot.user} is ready!")
+    
+    await bot.change_presence(activity = discord.Game("Keyword Monkey"))
+    playGame.start()
+
+@bot.command(name = "test")
+async def whats_good(context, user = "<@490277278136270874>"):
+
+    await context.reply(f"YOO WHATS GOOD {user}")
+
+@bot.command(pass_context = True)
+async def joinVoice(context):
+    
+    inVoice = context.author.voice
+
+    # If context.author.voice is not nil 
+    if (inVoice):
+        await inVoice.channel.connect()
+    
+    else:
+        await context.send("Not in channel")
+
+# Error handler for discord.ext bot
+@bot.event
+async def on_command_error(context, error):
+    
+    if isinstance(error, commands.MissingRequiredArgument):
+        await context.reply("I'm missing some information. Try again")
+
+    if isinstance(error, commands.CommandNotFound):
+        await context.reply("Not a valid command bozo")
 
 # MainSetup --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 bot.run(discordToken)
+
+# ReadNotes --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+# We can add defalut parameters if the user forgets to input one
+
+# context.reply = replys to the message.author
+# context.send = sends a regular message
+
+# bot.latency = gives your ping time (in seconds)
+
+# name = custom call of function (one way to call)
+# aliases = multiple ways to call same function
+
+# You can use fileName[-integerNumber] to remove characters off a string
