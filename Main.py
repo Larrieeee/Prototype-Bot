db65ef97992a87d7e95c88f47ea30e78eabc88da
Larry Le <lle114@student.hbuhsd.edu>
2022-03-03 17:48:20 -0800

    Implement Slash Commands

--- Main.py
+++ Main.py
@@ -1,6 +1,7 @@
 # Variables --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 import nextcord
+from nextcord import Interaction
 from nextcord.ext import commands
 
 from dotenv import load_dotenv
@@ -23,6 +24,14 @@ async def whats_good(context, *good):
     await context.reply(f"YOOOOOOOOOOO WHATS GOOD <@{context.author.id}>")
     await context.message.add_reaction("<:hehehehaw:945416057626165308>")
 
+# Slash Commands --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+@bot.slash_command(name = "indirect_ping", description = "Ping some guy", guild_ids = [586680405302968321])
+async def target_ping(interaction: Interaction, message:str):
+
+    await interaction.response.send_message(f"{message}")
+
 # MainSetup --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 bot.run(discordToken)
+# Note that the bot runs under the invite "applications.command"
